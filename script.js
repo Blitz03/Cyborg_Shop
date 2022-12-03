0cd1a3ab072950a9c8601723bf16bf6af8ad0f4a
Mustafa <mrmrmustafag77@gmail.com>
2022-12-03 20:34:18 +0300

    Fixing Bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const menuIcon = document.querySelector(".menu-icon i");
-const menuCloseIcon = document.querySelector(".menu-close-icon i");
-const menuList = document.querySelector(".menu-list");
+let menuIcon = document.querySelector(".menu-icon i");
+let menuCloseIcon = document.querySelector(".menu-close-icon i");
+let menuList = document.querySelector(".menu-list");
 
 menuIcon.addEventListener("click", () => {
   menuIcon.style.display = "none";
@@ -15,15 +15,15 @@ menuCloseIcon.addEventListener("click", () => {
   menuList.style.cssText = "visibility: hidden; opacity: 0;";
 });
 
-const cart = document.querySelector(".cart");
-const cartIcon = document.querySelector(".cart-icon");
+let cart = document.querySelector(".cart");
+let cartIcon = document.querySelector(".cart-icon");
 
 cartIcon.addEventListener("click", () => {
   cart.style.cssText = "visibility: visible; opacity: 1; top: 100%;";
 });
 
-const menuCart = document.querySelector(".menu-cart");
-const cartClose = document.querySelector(".close");
+let menuCart = document.querySelector(".menu-cart");
+let cartClose = document.querySelector(".close");
 
 cartClose.addEventListener("click", () => {
   cart.style.cssText = "visibility: hidden; opacity: 0; top: -100%;";
@@ -36,7 +36,7 @@ menuCart.addEventListener("click", () => {
   menuList.style.cssText = "visibility: hidden; opacity: 0;";
 });
 
-const removeIcons = document.querySelectorAll(".remove-icon i");
+let removeIcons = document.querySelectorAll(".remove-icon i");
 
 for (let i = 0; i < removeIcons.length; i++) {
   let button = removeIcons[i];
@@ -49,60 +49,67 @@ function removeFromCart(event) {
   updateCartTotal();
 };
 
-const addIcons = document.querySelectorAll(".products-cart-icon i");
+let quantities = document.querySelectorAll(".cart-box input");
+for (let i = 0; i < quantities.length; i++) {
+  let input = quantities[i];
+  input.addEventListener("change", updateCartTotal);
+};
+
+let addIcons = document.querySelectorAll(".products-cart-icon i");
 for (let i = 0; i < addIcons.length; i++) {
   let button = addIcons[i];
   button.addEventListener("click", addToCartClicked);
 };
 
 function addToCartClicked(event) {
-  const button = event.target;
-  const productBox = button.parentElement.parentElement.parentElement;
-  const imgSrc = productBox.querySelector("img").src;
-  const title = productBox.querySelector("h3").innerText;
-  const price = productBox.querySelector("span").innerText;
+  let button = event.target;
+  let productBox = button.parentElement.parentElement.parentElement;
+  let imgSrc = productBox.querySelector("img").src;
+  let title = productBox.querySelector("h3").innerText;
+  let price = productBox.querySelector("span").innerText;
   addToCart(imgSrc, title, price);
   updateCartTotal();
 };
 
 function addToCart(imgSrc, title, price) {
-  const cartImgSrc = document.querySelectorAll(".cart-box img");
-  const quantityInputs = document.querySelectorAll(".cart-box input");
+  let cartImgSrc = document.querySelectorAll(".cart-box img");
+  let quantityInputs = document.querySelectorAll(".cart-box input");
   for (let i = 0; i < cartImgSrc.length; i++) {
     if (cartImgSrc[i].src === imgSrc ) {
       quantityInputs[i].value++;
       return;
     };
   };
-  const newBox = document.createElement("div");
-  const cartBoxes = document.querySelector(".cart-boxes");
+  let newBox = document.createElement("div");
+  let cartBoxes = document.querySelector(".cart-boxes");
   newBox.classList.add("cart-box");
-  cartBoxes.appendChild(newBox);
-  const newBoxContent = `
+  let newBoxContent = `
   <img src="${imgSrc}" alt="">
   <div class="details">
-    <h4>${title}</h4>
-    <span>${price}</span>
-    <input type="number" value="1" min="1">
+  <h4>${title}</h4>
+  <span>${price}</span>
+  <input type="number" value="1" min="1">
   </div>
   <div class="remove-icon">
-    <i class="fa-solid fa-trash"></i>
+  <i class="fa-solid fa-trash"></i>
   </div>
   `
   newBox.innerHTML = newBoxContent;
+  cartBoxes.appendChild(newBox);
   newBox.querySelector(".remove-icon i").addEventListener("click", removeFromCart);
   newBox.querySelector(".cart-box input").addEventListener("change", updateCartTotal);
 };
 
 function updateCartTotal() {
   let total = 0;
-  const cartBoxes = document.querySelectorAll(".cart-box");
+  let cartBoxes = document.querySelectorAll(".cart-box");
   for (let i = 0; i < cartBoxes.length; i++) {
-    const cartBox = cartBoxes[i];
-    const priceElement = cartBox.querySelector("span").innerText.replace("$", "");
-    const price = parseFloat(priceElement);
-    const quantity = cartBox.querySelector("input").value;
+    let cartBox = cartBoxes[i];
+    let priceElement = cartBox.querySelector("span").innerHTML.replace("$", "");
+    let price = parseFloat(priceElement);
+    let quantity = cartBox.querySelector("input").value;
     total = total + (price * quantity);
+    console.log(total);
   };
   document.querySelector(".subtotal-number").innerText = "$" + total;
 };
